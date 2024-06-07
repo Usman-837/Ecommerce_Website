55272a60f91aa5e6268b438727ed63589b2a0864
Usman-837 <usmantufail837@gmail.com>
2024-06-07 16:12:03 +0500

    Add Lower Navbar and DrakMode Session

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import Navbar from "./Navbar";
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
 
 const App = () => {
+  const [orderPopup, setOrderPopup] = React.useState(false);
+
+  const handleOrderPopup = () => {
+    setOrderPopup(!orderPopup);
+  };
+
   return (
-    <>
-      <Navbar/>    
-    </>
+    <div className="bg-white dark:bg-gray-900 dark:text-white duration-200">
+      <Navbar handleOrderPopup={handleOrderPopup} />
+      <Hero/>
+    </div>
   );
 };
 
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Image1 from "../../assets/hero/women.png";
+import Image2 from "../../assets/hero/shopping.png";
+import Image3 from "../../assets/hero/sale.png";
+
+const ImageList = [
+    {
+        id: 1,
+        img: Image1,
+        title: "Upto 50% off on all Men's Wear",
+        description: 
+            "Loremslcjscbsd bdj kv svfdgvdjvbvj ,b; dv ;sg bv.dvhj vkvhdv kh kdvvfkdfb vfdkjsbv",
+    },
+    {
+        id: 2,
+        img: Image2,
+        title: "Upto 30% off on all Women's Wear",
+        description: 
+            "Loremslcjscbsd bdj kv svfdgvdjvbvj ,b; dv ;sg bv.dvhj vkvhdv kh kdvvfkdfb vfdkjsbv",
+    },
+    {
+        id: 3,
+        img: Image3,
+        title: "70% off on all Products Sale",
+        description: 
+            "Loremslcjscbsd bdj kv svfdgvdjvbvj ,b; dv ;sg bv.dvhj vkvhdv kh kdvvfkdfb vfdkjsbv",
+    },
+];
+
+
+const Hero = () => {
+    return (
+        <div>Hero Session</div>
+    );
+}
+
+export default Hero;
