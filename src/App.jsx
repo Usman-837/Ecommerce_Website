9d5ba1d9acbed054ad7d97209d076936c683ab1f
Usman-837 <usmantufail837@gmail.com>
2024-06-08 12:45:48 +0500

    Create Products Component and Added Header session in it

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
+import Products from "./components/Products/Products";
 
 const App = () => {
   const [orderPopup, setOrderPopup] = React.useState(false);
@@ -13,6 +14,7 @@ const App = () => {
     <div className="bg-white dark:bg-gray-900 dark:text-white duration-200">
       <Navbar handleOrderPopup={handleOrderPopup} />
       <Hero/>
+      <Products/>
     </div>
   );
 };
---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Products = () => {
+  return (
+    <div className="mt-14 mb-12">
+      <div className="container">
+        {/* Header Session */}
+        <div className="text-center mb-10 max-w-[600px] mx-auto">
+            <p className="text-sm text-primary">Top Selling Products for you</p>
+            <h1 className="text-3xl font-bold">Products</h1>
+            <p className="text-xs text-gray-400">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem suscipit ut consectetur enim fuga quaerat esse repellendus eligendi tempore culpa nulla, quas cupiditate alias fugit odio labore soluta illum dolore.
+            </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Products;
