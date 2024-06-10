41aff9ed63dbe14d8c1366153b2bd83c818f8c81
Usman-837 <usmantufail837@gmail.com>
2024-06-10 15:31:16 +0500

    Added one more Products Section

--- src/App.jsx
+++ src/App.jsx
@@ -34,6 +34,7 @@ const App = () => {
       <TopProducts/>
       <Banner/>
       <Subscribe/>
+      <Products/>
     </div>
   );
 };
--- src/components/TopProducts/TopProducts.jsx
+++ src/components/TopProducts/TopProducts.jsx
@@ -78,6 +78,7 @@ const TopProducts = () => {
                     </div>
                 </div>
             ))}
+            
         </div>
       </div>
     </div>
