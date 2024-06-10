4b139c017974131773a52bee7a4db6d83acdaf69
Usman-837 <usmantufail837@gmail.com>
2024-06-10 18:58:04 +0500

    Added Footer Section

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -103,12 +103,12 @@ const Testimonials = () => {
                   <div className="flex flex-col items-center gap-4">
                     <div className="space-y-3">
                       <p className="text-xs text-gray-500">{data.text}</p>
-                      <h1 className="text-xl font-bold text-black/80 dark:text-light">
+                      <h1 className="text-xl font-bold text-black/80 dark:text-light dark:text-white">
                         {data.name}
                       </h1>
                     </div>
                   </div>
-                  <p className="text-black/20 text-9xl font-serif absolute top-0 right-0">
+                  <p className="text-black/20 text-9xl font-serif absolute top-0 right-0 dark:text-white">
                     ,,
                   </p>
                 </div>
