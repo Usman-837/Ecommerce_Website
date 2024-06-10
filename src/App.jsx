49b20ae7dd5f5c0340ea0cc05ca253b940113adc
Usman-837 <usmantufail837@gmail.com>
2024-06-10 14:35:27 +0500

    Added Subscribe Section

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 import TopProducts from "./components/TopProducts/TopProducts";
 import Banner from "./components/Banner/Banner";
+import Subscribe from "./components/Subscribe/Subscribe";
 
 const App = () => {
 
@@ -32,6 +33,7 @@ const App = () => {
       <Products/>
       <TopProducts/>
       <Banner/>
+      <Subscribe/>
     </div>
   );
 };
