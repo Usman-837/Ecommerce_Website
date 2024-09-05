b515a3dd79b8af8a4fb0507e23f3e864df99cae6
Usman-837 <usmantufail837@gmail.com>
2024-09-05 23:18:56 +0500

    add menu bar with toggle functionality

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../../assets/logo.png";
 import { IoMdSearch } from "react-icons/io";
 import { FaCartShopping } from "react-icons/fa6";
 import { FaCaretDown } from "react-icons/fa";
 import DarkMode from "./DarkMode";
+import { FiMenu } from "react-icons/fi";
 
 const Menu = [
   {
@@ -57,14 +58,19 @@ const DropdownLinks = [
 ];
 
 const Navbar = ({ handleOrderPopup }) => {
+  const [menu, setMenu] = useState(false);
+
   return (
     <>
-      <div className="shadow-md bg-white dark:bg-gray-900 dark:text-white duration-200 fixed top-0 left-0 w-full z-50">
+      <div className="shadow-md bg-white dark:bg-gray-900 dark:text-white duration-200 fixed w-full z-50">
         {/* upper Navbar */}
         <div className="bg-primary/40 py-2">
           <div className="container flex justify-between items-center">
             <div>
-              <a href="#hero" className="font-bold text-2xl sm:text-3xl flex gap-2">
+              <a
+                href="#hero"
+                className="font-bold text-2xl sm:text-3xl flex gap-2"
+              >
                 <img src={Logo} alt="Logo" className="w-10" />
                 Shopsy
               </a>
@@ -72,7 +78,7 @@ const Navbar = ({ handleOrderPopup }) => {
 
             {/* search bar */}
             <div className="flex justify-between items-center gap-4">
-              <div className="group relative hidden sm:block">
+              <div className="group relative max-md:hidden">
                 <input
                   type="text"
                   placeholder="search"
@@ -96,12 +102,20 @@ const Navbar = ({ handleOrderPopup }) => {
               <div>
                 <DarkMode />
               </div>
+              <div>
+                <FiMenu
+                  className="text-[#ed9821] dark:text-white size-7 hidden max-md:block"
+                  onClick={() => {
+                    setMenu(!menu);
+                  }}
+                />
+              </div>
             </div>
           </div>
         </div>
         {/* lower Navbar */}
-        <div data-aos="zoom-in" className="flex justify-center">
-          <ul className="sm:flex hidden items-center gap-4">
+        <div data-aos="zoom-in" className="flex justify-center max-md:hidden">
+          <ul className="flex items-center gap-4 max-lg:gap-1">
             {Menu.map((data) => (
               <li key={data.id}>
                 <a
@@ -137,8 +151,52 @@ const Navbar = ({ handleOrderPopup }) => {
             </li>
           </ul>
         </div>
+        {/* Toggle lower Navbar */}
+        {menu ? (
+          <div data-aos="zoom-in" className="hidden max-md:block">
+            <ul className="py-4 pl-4 text-lg">
+              {Menu.map((data) => (
+                <li
+                  key={data.id}
+                  className="py-1"
+                  onClick={() => setMenu(!menu)}
+                >
+                  <a
+                    href={data.link}
+                    className="inline-block px-4 hover:text-primary duration-200"
+                  >
+                    {data.name}
+                  </a>
+                </li>
+              ))}
+              {/* Simple Dropdown and Links */}
+              <li className="group relative cursor-pointer">
+                <a href="#" className="flex items-center pl-4 py-1">
+                  Trending Products
+                  <span>
+                    <FaCaretDown className="transition-all duration-200 group-hover:rotate-180" />
+                  </span>
+                </a>
+                <div className="absolute z-[9999] hidden group-hover:block w-[200px] rounded-md bg-white p-2 text-black shadow-md">
+                  <ul>
+                    {DropdownLinks.map((data) => (
+                      <li key={data.id} onClick={() => setMenu(!menu)}>
+                        <a
+                          href={data.link}
+                          className="inline-block w-full rounded-md p-2 hover:bg-primary/20"
+                        >
+                          {data.name}
+                        </a>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </li>
+            </ul>
+          </div>
+        ) : null}
       </div>
-      <div className="pt-[100px]">
+      <div className="pt-[80px] max-md:pt-0">
         {/* Add some padding to prevent content from being hidden behind the navbar */}
       </div>
     </>
